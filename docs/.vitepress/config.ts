6c13518271bffd44c988a800baa59a291d345247
ltucat <ltucat@proton.me>
2023-08-12 12:58:54 -0400

    add chapter 2,3,4

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "反共产主义者宣言",
+  title: "反共产主义宣言",
   description: "",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -28,10 +28,35 @@ export default defineConfig({
         text: "第一章：静态和动态的阶级",
         link: "/1-static-dynamic-classes",
       },
+      {
+        text: "第二章：有能力的个人",
+        link: "/2-competent-individual",
+      },
+      {
+        text: "第三章：资本主义的幽灵变种",
+        link: "/3-capitalism-variation",
+      },
+      {
+        text: "第四章：个人与国家的关系",
+        link: "/4-individual-state",
+      },
+      {
+        text: "后序",
+        link: "/afterword",
+      },
+      {
+        text: "关于作者",
+        link: "/about-authors",
+      },
+      {
+        text: "补充内容",
+        link: "/bonus-materials",
+      },
     ],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/ltucat/uncommunist" },
+      { icon: "twitter", link: "https://twitter.com/@ltucat" },
     ],
   },
 });
--- docs/index.md
+++ docs/index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: "反共产主义者宣言"
+  name: "反共产主义宣言"
   text: ""
   tagline: "<br/>作者：Aleks Svetski & Mark Moss<br/><br/>本书没有版权。<br/><br/>思想并非知识产权。因此，请广泛地分享和传播本书。盗版也可以，让更多的人看到它。如果您发现了它的价值，请随意购买一些来支持我们。<br/><br/>原书《The UnCommunist Manifesto》"
 
