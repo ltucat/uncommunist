45fea66e7c532489ff86b23545ddfb32037f96f9
ltucat <ltucat@proton.me>
2024-08-11 15:08:30 -0400

    adjust book folder structure and add new book

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,62 +1,147 @@
 import { defineConfig } from "vitepress";
 
+const sidebarUncommunist = () => {
+  return [
+    {
+      text: "反共产主义宣言",
+      items: [
+        {
+          text: "致谢",
+          link: "/acknowledgments",
+        },
+        {
+          text: "前言",
+          link: "/preface",
+        },
+        {
+          text: "术语定义",
+          link: "/definitions",
+        },
+        {
+          text: "第一章：静态和动态的阶级",
+          link: "/1-static-dynamic-classes",
+        },
+        {
+          text: "第二章：有能力的个人",
+          link: "/2-competent-individual",
+        },
+        {
+          text: "第三章：资本主义的幽灵变种",
+          link: "/3-capitalism-variation",
+        },
+        {
+          text: "第四章：个人与国家的关系",
+          link: "/4-individual-state",
+        },
+        {
+          text: "后序",
+          link: "/afterword",
+        },
+        {
+          text: "关于作者",
+          link: "/about-authors",
+        },
+        {
+          text: "补充内容",
+          link: "/bonus-materials",
+        },
+      ],
+    },
+  ];
+};
+
+const sidebarMaosAmerica = () => {
+  return [
+    {
+      text: "毛泽东式的美国",
+      items: [
+        {
+          text: "序",
+          link: "/preface",
+        },
+        {
+          text: "前言",
+          link: "/introduction",
+        },
+        {
+          text: "第一章：离开厨房的餐桌",
+          link: "/01",
+        },
+        {
+          text: "第二章：在毛统治的中国长大",
+          link: "/02",
+        },
+        {
+          text: "第三章：自由之地",
+          link: "/03",
+        },
+        {
+          text: "第四章：两次文化大革命",
+          link: "/04",
+        },
+        {
+          text: "第五章：生而有罪 - 意识形态是如何分裂的",
+          link: "/05",
+        },
+        {
+          text: "第六章：红卫兵 - 革命的突击队",
+          link: "/06",
+        },
+        {
+          text: "第七章：文化清洗 - 一场对旧世界的战争",
+          link: "/07",
+        },
+        {
+          text: "第八章：家庭结构的解体",
+          link: "/08",
+        },
+        {
+          text: "第九章：宗教信仰的消亡",
+          link: "/09",
+        },
+        {
+          text: "第十章：塑造新人 - 思想意识的灌输",
+          link: "/10",
+        },
+        {
+          text: "后记：一个警告",
+          link: "/epilogue",
+        },
+        {
+          text: "致谢",
+          link: "/acknowledgements",
+        },
+      ],
+    },
+  ];
+};
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "反共产主义宣言",
-  description: "",
+  title: "反共产主义",
+  description: "Communism never failed to fail",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "首页", link: "/" },
-      { text: "开始阅读", link: "/acknowledgments" },
+      {
+        text: "反共产主义宣言",
+        link: "/uncommunist/acknowledgments",
+      },
+      /*{
+        text: "毛泽东式的美国",
+        link: "/maosamerica/preface",
+      },*/
     ],
 
-    sidebar: [
-      {
-        text: "致谢",
-        link: "/acknowledgments",
-      },
-      {
-        text: "前言",
-        link: "/preface",
-      },
-      {
-        text: "术语定义",
-        link: "/definitions",
-      },
-      {
-        text: "第一章：静态和动态的阶级",
-        link: "/1-static-dynamic-classes",
-      },
-      {
-        text: "第二章：有能力的个人",
-        link: "/2-competent-individual",
-      },
-      {
-        text: "第三章：资本主义的幽灵变种",
-        link: "/3-capitalism-variation",
-      },
-      {
-        text: "第四章：个人与国家的关系",
-        link: "/4-individual-state",
-      },
-      {
-        text: "后序",
-        link: "/afterword",
-      },
-      {
-        text: "关于作者",
-        link: "/about-authors",
-      },
-      {
-        text: "补充内容",
-        link: "/bonus-materials",
-      },
-    ],
+    sidebar: {
+      "/uncommunist/": { base: "/uncommunist", items: sidebarUncommunist() },
+      "/maosamerica/": { base: "/maosamerica", items: sidebarMaosAmerica() },
+    },
 
     socialLinks: [
       { icon: "github", link: "https://github.com/ltucat/uncommunist" },
-      { icon: "twitter", link: "https://twitter.com/@ltucats" },
+      { icon: "twitter", link: "https://x.com/@ltu1111" },
     ],
   },
 });
--- docs/index.md
+++ docs/index.md
@@ -3,14 +3,15 @@
 layout: home
 
 hero:
-  name: "反共产主义宣言"
-  text: ""
-  tagline: "<br/>作者：Aleks Svetski & Mark Moss<br/><br/>本书没有版权。<br/><br/>思想并非知识产权。因此，请广泛地分享和传播本书。盗版也可以，让更多的人看到它。如果您发现了它的价值，请随意购买一些来支持我们。<br/><br/>原书《The UnCommunist Manifesto》"
+  name: "反共产主义"
+  tagline: "“自由随时可能消失，仅仅一代人的时间就足以让它灭绝。自由不是与生俱来的，不会自然而然地传承给下一代。我们必须为之奋斗，坚决捍卫，并教育下一代同样去珍惜和守护。” — 罗纳德·里根总统"
 
-  actions:
-    - theme: brand
-      text: 开始阅读
-      link: /acknowledgments
-
+features:
+- details: "<div><img src='/uncommunist/public/book-cover.jpg' style='float:right; width:100px; padding:5px;'/> <h2 style='padding-bottom:10px; font-size:1.4em;'><strong>《反共产主义宣言》</strong></h2> <p>作者：Aleks Svetski & Mark Moss<br/>作者声明：本书没有版权。思想并非知识产权。因此，请广泛地分享和传播本书。盗版也可以，让更多的人看到它。如果您发现了它的价值，请购买支持原书《<a href='https://www.amazon.com/UnCommunist-Manifesto-Message-Responsibility-Liberty-ebook/dp/B0B837FN63' target='_blank'>The UnCommunist Manifesto</a>》。</p></div>"
+  link: /uncommunist/acknowledgments
+  linkText: 开始阅读
+#- details: "<div><img src='/maosamerica/public/book-cover.jpg' style='float:right; width:100px; padding:5px;'/> <h2 style='padding-bottom:10px; font-size:1.4em;'><strong>《毛泽东式的美国》</strong></h2> <p>作者：Xi Van Fleet<br/>一位从中国文化大革命中幸存者对美国的警告：历史正在以令人不安的方式重演。</p><p>请支持购买原书《<a href='https://www.amazon.com/Maos-America-Survivors-Van-Fleet-ebook/dp/B0BWY3JV76' target='_blank'>Mao's America</a>》。</p></div>"
+#  link: /maosamerica/preface
+#  linkText: 开始阅读
 ---
 
